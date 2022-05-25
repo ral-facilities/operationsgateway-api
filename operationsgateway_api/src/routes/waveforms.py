9e818a413d4a2e586c048461b7f7e4a92a048bec
Matthew Richards <matthew.richards@stfc.ac.uk>
2022-05-25 10:26:16 +0000

    Implement get waveforms by ID endpoint

--- operationsgateway_api/src/routes/waveforms.py
+++ operationsgateway_api/src/routes/waveforms.py
@@ -2,6 +2,11 @@ import logging
 
 from fastapi import APIRouter
 
+from operationsgateway_api.src.data_encoding import DataEncoding
+from operationsgateway_api.src.models import Record
+
+from operationsgateway_api.src.mongo.interface import MongoDBInterface
+
 
 log = logging.getLogger()
 router = APIRouter()
@@ -9,6 +14,14 @@ router = APIRouter()
 
 @router.get("/waveforms/{id_}", response_description="")
 async def get_waveform_by_id(id_: str):
-    pass
+    log.info("Getting waveform by ID: %s", id_)
+
+    waveform = await MongoDBInterface.find_one(
+        "waveforms",
+        {"_id": DataEncoding.encode_object_id(id_)},
+    )
+
+    # TODO - need to make that model more generic, not specific to records
+    return Record.construct(waveform.keys(), **waveform)
 
 # TODO - do we want /waveforms/count
