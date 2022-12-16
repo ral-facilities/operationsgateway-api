8d478e6844f6e077a963e050d271a2c429db24a3
Matthew Richards <matthew.richards@stfc.ac.uk>
2022-12-16 10:11:58 +0000

    DSEGOG-165 Add working tests for waveform and image channel summaries

--- test/endpoints/test_channel_summary.py
+++ test/endpoints/test_channel_summary.py
@@ -1,3 +1,5 @@
+import base64
+import hashlib
 from fastapi.testclient import TestClient
 import pytest
 
@@ -28,27 +30,9 @@ class TestChannelSummary:
                 },
                 id="Scalar channel (string) summary",
             ),
-            # pytest.param(
-            #    "N_COMP_FF_IMAGE",
-            #    {
-            #        "first_date": "2022-04-07T14:16:16",
-            #        "most_recent_date": "2022-04-08T16:58:57",
-            #        "recent_sample": [],
-            #    },
-            #    id="Image channel summary",
-            # ),
-            # pytest.param(
-            #    "N_COMP_SPEC_TRACE",
-            #    {
-            #        "first_date": "2022-04-07T14:16:16",
-            #        "most_recent_date": "2022-04-08T16:58:57",
-            #        "recent_sample": [],
-            #    },
-            #    id="Waveform channel summary",
-            # ),
         ],
     )
-    def test_valid_channel_summary(
+    def test_valid_scalar_channel_summary(
         self,
         test_app: TestClient,
         login_and_get_token,
@@ -62,6 +46,64 @@ class TestChannelSummary:
 
         assert test_response.status_code == 200
         assert test_response.json() == expected_summary
+    
+    @pytest.mark.parametrize(
+        "channel_name, expected_summary",
+        [
+            pytest.param(
+                "N_COMP_FF_IMAGE",
+                {
+                    "first_date": "2022-04-07T14:16:16",
+                    "most_recent_date": "2022-04-08T16:58:57",
+                    "recent_sample": [
+                        "bdd30e16457f13cd777209a497e5fb0b",
+                        "9cf74f5aec5fe857bad5a79c3ed5e0e8",
+                        "10ace7b97cba6d986c5b60250d360988",
+                    ],
+                },
+                id="Image channel summary",
+            ),
+            pytest.param(
+                "N_COMP_SPEC_TRACE",
+                {
+                    "first_date": "2022-04-07T14:16:16",
+                    "most_recent_date": "2022-04-08T16:58:57",
+                    "recent_sample": [
+                        "76b91b973c87b877243d237a84c7d40c",
+                        "e30e17ca5249278431cafdb1a250e73c",
+                        "3f542031460398a672779bca52a619a8",
+                    ],
+                },
+                id="Waveform channel summary",
+            ),
+        ]
+    )
+    def test_valid_thumbnail_channel_summary(
+        self,
+        test_app: TestClient,
+        login_and_get_token,
+        channel_name,
+        expected_summary,
+    ):
+
+        """
+        Compare the response with the expected result, but convert the returned base64
+        thumbnails to an MD5 checksum beforehand (to prevent bloating this file with
+        long base64 strings)
+        """
+
+        test_response = test_app.get(
+            f"/channels/summary/{channel_name}",
+            headers={"Authorization": f"Bearer {login_and_get_token}"},
+        )
+
+        assert test_response.status_code == 200
+        json_output = test_response.json()
+        for i in range(len(json_output["recent_sample"])):
+            bytes_thumbnail = base64.b64decode(json_output["recent_sample"][i])
+            json_output["recent_sample"][i] = hashlib.md5(bytes_thumbnail).hexdigest()  # noqa: S303
+
+        assert json_output == expected_summary
 
     @pytest.mark.parametrize(
         "channel_name",
