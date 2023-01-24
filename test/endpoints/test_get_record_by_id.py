d91220c9cbeac61a0678420475f621c435c850bd
kevinphippsstfc <kevinphippsstfc@users.noreply.github.com>
2023-01-24 15:00:17 +0000

    Add test to check false colour thumnails

--- test/endpoints/test_get_record_by_id.py
+++ test/endpoints/test_get_record_by_id.py
@@ -3,7 +3,7 @@ import json
 from fastapi.testclient import TestClient
 import pytest
 
-from test.conftest import assert_record
+from test.conftest import assert_record, assert_thumbnails
 
 
 class TestGetRecordByID:
@@ -39,4 +39,37 @@ class TestGetRecordByID:
             test_response.json(),
             expected_channel_count,
             expected_channel_data,
+        )
+
+    @pytest.mark.parametrize(
+        "record_id, expected_thumbnail_md5s",
+        [
+            pytest.param(
+                "20220408132830",
+                {
+                    "N_COMP_FF_IMAGE": "c51b2889631ac3b715a6d93e8059bfa7",
+                    "N_COMP_NF_IMAGE": "444e28f99fe9ce9b9e754aa270ad644e",
+                    "N_LEG1_GREEN_NF_IMAGE": "bc110f66cd0260a21734ae63ac59fc5d",
+                },
+                id="Ordinary request",
+            ),
+        ],
+    )
+    def test_image_thumbnails_get_record_by_id(
+        self,
+        test_app: TestClient,
+        login_and_get_token,
+        record_id,
+        expected_thumbnail_md5s,
+    ):
+        test_response = test_app.get(
+            f"/records/{record_id}",
+            headers={"Authorization": f"Bearer {login_and_get_token}"},
+        )
+
+        assert test_response.status_code == 200
+
+        assert_thumbnails(
+            test_response.json(),
+            expected_thumbnail_md5s,
         )
