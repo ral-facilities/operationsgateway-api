64d9e115bcd8fcbf3a589765c59c6015611f1c28
william cross <william.cross@stfc.ac.uk>
2023-11-13 09:21:08 +0000

    DSEGOG-268 finished all requested changes on test_delete_users
    combined 2 tests into one using parametrize twice
    changed how the parametrized mark that was originaly in the file to the standard form

--- test/endpoints/test_delete_users.py
+++ test/endpoints/test_delete_users.py
@@ -3,73 +3,81 @@ import pytest
 
 
 class TestDeleteUsers:
+    @pytest.mark.parametrize(
+        "username",
+        [
+            pytest.param(
+                "/users/testuserthatdoesnotexistinthedatabasefed",
+                id="delete fed user success",
+            ),
+            pytest.param(
+                "/users/testuserthatdoesnotexistinthedatabaselocal",
+                id="delete local user success",
+            ),
+        ],
+    )
     @pytest.mark.asyncio
-    async def test_delete_local_user_success(
+    async def test_delete_user_success(
         self,
         test_app: TestClient,
         login_and_get_token,
         add_delete_local_fixture,
+        add_delete_fed_fixture,
+        username,
     ):
         delete_local_response = test_app.delete(
-            "/users/testuserthatdoesnotexistinthedatabaselocal",
+            username,
             headers={"Authorization": f"Bearer {login_and_get_token}"},
         )
 
         assert delete_local_response.status_code == 204
 
-    @pytest.mark.asyncio
-    async def test_delete_fed_user_success(
-        self,
-        test_app: TestClient,
-        login_and_get_token,
-        add_delete_fed_fixture,
-    ):
-        delete_fed_response = test_app.delete(
-            "/users/testuserthatdoesnotexistinthedatabasefed",
-            headers={"Authorization": f"Bearer {login_and_get_token}"},
-        )
-
-        assert delete_fed_response.status_code == 204
-
     @pytest.mark.parametrize(
         "username",
-        ["testuserthatdoesnotexistinthedatabase"],
-        ids=["test_delete_user_fail"],
+        [
+            pytest.param(
+                "/users/testuserthatdoesnotexistinthedatabase",
+                id="test_delete_user_fail",
+            ),
+        ],
     )
     @pytest.mark.asyncio
     async def test_delete_user_fail(
         self,
-        username: str,
         test_app: TestClient,
         login_and_get_token,
+        username,
     ):
         delete_response = test_app.delete(
-            "/users/testuserthatdoesnotexistinthedatabase",
+            username,
             headers={"Authorization": f"Bearer {login_and_get_token}"},
         )
 
         assert delete_response.status_code == 500
 
+    @pytest.mark.parametrize(
+        "username",
+        [
+            pytest.param(
+                "/users/testuserthatdoesnotexistinthedatabasefed",
+                id="delete fed user forbidden",
+            ),
+            pytest.param(
+                "/users/testuserthatdoesnotexistinthedatabaselocal",
+                id="delete local user forbidden",
+            ),
+        ],
+    )
     @pytest.mark.asyncio
-    async def test_delete_fed_user_forbidden(
-        self,
-        test_app: TestClient,
-        add_delete_fed_fixture,
-    ):
-        delete_fed_response = test_app.delete(
-            "/users/testuserthatdoesnotexistinthedatabasefed",
-        )
-
-        assert delete_fed_response.status_code == 403
-
-    @pytest.mark.asyncio
-    async def test_delete_local_user_forbidden(
+    async def test_delete_user_forbidden(
         self,
         test_app: TestClient,
         add_delete_local_fixture,
+        add_delete_fed_fixture,
+        username,
     ):
         delete_fed_response = test_app.delete(
-            "/users/testuserthatdoesnotexistinthedatabaselocal",
+            username,
         )
 
         assert delete_fed_response.status_code == 403
