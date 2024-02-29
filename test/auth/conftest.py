6e726a288cfd530179c3a1a2bec42417200c04cc
william cross <william.cross@stfc.ac.uk>
2024-02-29 10:41:22 +0000

    DSEGOG-262 increased code coverage on authentication.py
    could not cover both exceptions inside _do_fedid_auth
    as the exception changes depending on if your using the VPN or not

--- test/auth/conftest.py
+++ test/auth/conftest.py
@@ -2,14 +2,36 @@ import os
 
 import pytest
 
+from operationsgateway_api.src.auth.authentication import Authentication
 from operationsgateway_api.src.auth.jwt_handler import JwtHandler
-from operationsgateway_api.src.models import UserModel
+from operationsgateway_api.src.models import LoginDetailsModel, UserModel
 
 
 @pytest.fixture
 def jwt_handler_instance():
-    test_user = UserModel(_id="Test User", auth_type="Test")
+    test_user = UserModel(
+        _id="testuserthatdoesnotexistinthedatabaselocal",
+        auth_type="Test",
+    )
     return JwtHandler(test_user)
+
+
+@pytest.fixture
+def authentication_fed_instance():
+    test_user = UserModel(
+        _id="testuserthatdoesnotexistinthedatabasefed",
+        auth_type="FedID",
+    )
+    test_login_details = LoginDetailsModel(username="Test", password="password")
+    return Authentication(test_login_details, test_user)
+
+
+@pytest.fixture
+def authentication_local_instance():
+    password = "random_password"
+    test_user = UserModel(_id="Test User", auth_type="local", sha256_password=password)
+    test_login_details = LoginDetailsModel(username="backend", password=password)
+    return Authentication(test_login_details, test_user)
 
 
 @pytest.fixture
---
+++ test/auth/test_authentication.py
@@ -0,0 +1,33 @@
+from unittest.mock import patch
+
+import pytest
+
+from operationsgateway_api.src.config import Config
+from operationsgateway_api.src.exceptions import AuthServerError, UnauthorisedError
+
+
+class TestAuthentication:
+    def test_fed_server_problem(self, authentication_fed_instance):
+        with pytest.raises(Exception):
+            authentication_fed_instance.authenticate()
+
+    def test_local_unauthorised(self, authentication_local_instance):
+        with pytest.raises(UnauthorisedError):
+            authentication_local_instance.authenticate()
+
+    def test_ldap_server_fail(self, monkeypatch, authentication_fed_instance):
+        # this test should pass if you are on the site network or home network
+        monkeypatch.setattr(
+            Config.config.auth,
+            "fedid_server_ldap_realm",
+            "incorrect_field",
+        )
+        with pytest.raises((UnauthorisedError, AuthServerError)):
+            authentication_fed_instance.authenticate()
+
+    def test_fed_user_success(self, authentication_fed_instance):
+        with patch("ldap.initialize") as mock_initialize:
+            mock_conn = mock_initialize.return_value
+            with patch.object(mock_conn, "simple_bind_s") as mock_simple_bind_s:
+                mock_simple_bind_s.return_value = ""
+                authentication_fed_instance.authenticate()
