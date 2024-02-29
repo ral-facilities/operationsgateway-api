30c87cd6e5a85f028f886cf08f71f35be5ebe1eb
william cross <william.cross@stfc.ac.uk>
2024-02-29 14:32:26 +0000

    DSEGOG-262 increased code coverage for channel_manifest.py
    increased code coverage to 100% for that file

---
+++ test/channels/test_channel_manifest.py
@@ -0,0 +1,82 @@
+from tempfile import SpooledTemporaryFile
+from unittest.mock import patch
+
+import pytest
+
+from operationsgateway_api.src.channels.channel_manifest import ChannelManifest
+from operationsgateway_api.src.exceptions import ChannelManifestError, ModelError
+
+
+def get_spooled_file(fail_type=0):
+    spooled_file = SpooledTemporaryFile()
+
+    if fail_type == 1:
+        content = (
+            '{"_id": "19830222132431, "channels": {"PM-201-FE-CAM-1": '
+            '{"name": "D100 front-end NF", "path": "/PM-201/FE", "type": "image"}, '
+            '"PM-201-FE-CAM-2": {"name": "D100 front-end FF", "path": "/PM-201/FE",'
+            ' "type": "image"}}}'
+        )
+    elif fail_type == 2:
+        content = "{}"
+    else:
+        content = (
+            '{"_id": "19830222132431", "channels": {"PM-201-FE-CAM-1": '
+            '{"name": "D100 front-end NF", "path": "/PM-201/FE", "type": "image"}, '
+            '"PM-201-FE-CAM-2": {"name": "D100 front-end FF", "path": "/PM-201/FE",'
+            ' "type": "image"}}}'
+        )
+    spooled_file.write(content.encode())
+    spooled_file.seek(0)
+
+    return ChannelManifest(manifest_input=spooled_file)
+
+
+class TestChannelManifest:
+    @pytest.mark.asyncio
+    async def test_get_channel_fail(self):
+        with pytest.raises(
+            ChannelManifestError,
+            match="Channel 'non_existant_channel' cannot be found",
+        ):
+            await ChannelManifest.get_channel(channel_name="non_existant_channel")
+
+    @pytest.mark.asyncio
+    async def test_decode_error(self):
+        with pytest.raises(
+            ChannelManifestError,
+            match="Channel manifest file not valid JSON",
+        ):
+            get_spooled_file(fail_type=1)
+
+    @pytest.mark.asyncio
+    async def test_manifest_validation_error(self):
+        with pytest.raises(ModelError):
+            get_spooled_file(fail_type=2)
+
+    @pytest.mark.asyncio
+    async def test_validate_pass(self):
+        instance = get_spooled_file()
+
+        await instance.validate(bypass_channel_check=True)
+
+    @pytest.mark.asyncio
+    async def test_validate_no_stored_file(self):
+        instance = get_spooled_file()
+
+        with patch(
+            "operationsgateway_api.src.channels.channel_manifest.ChannelManifest.get_most_recent_manifest",
+            return_value=None,
+        ):
+            await instance.validate(bypass_channel_check=True)
+
+    @pytest.mark.asyncio
+    async def test_insert_success(self, remove_manifest_entry):
+        with patch(
+            "operationsgateway_api.src.channels.channel_manifest.ChannelManifest._add_id",
+            return_value="19830222132431",
+        ):
+
+            instance = get_spooled_file()
+
+            await instance.insert()
