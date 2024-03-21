de3b06f872697f7397f6b26af803d259c589e3bd
william cross <william.cross@stfc.ac.uk>
2024-03-21 13:57:24 +0000

    DSEGOG-262 increased code coverage on records
    added integration tests for routes/records.py

---
+++ test/endpoints/test_delete_record.py
@@ -0,0 +1,42 @@
+from fastapi.testclient import TestClient
+import pytest
+
+from operationsgateway_api.src.records.record import Record
+
+
+class TestDeleteRecordById:
+    @pytest.mark.asyncio
+    async def test_delete_record_success(
+        self,
+        test_app: TestClient,
+        login_and_get_token,
+    ):
+        record_id = 19000000000011
+        test_record = {
+            "_id": "19000000000011",
+            "metadata": {
+                "epac_ops_data_version": "1.0",
+                "shotnum": 423648000000,
+                "timestamp": "2023-06-05T08:00:00",
+            },
+            "channels": {
+                "test_scalar_id": {
+                    "metadata": {"channel_dtype": "scalar", "units": "µm"},
+                    "data": 5.126920467610521,
+                },
+            },
+        }
+
+        record_instance = Record(test_record)
+        record_instance.insert()
+
+        delete_response = test_app.delete(
+            f"/records/{record_id}",
+            headers={"Authorization": f"Bearer {login_and_get_token}"},
+        )
+
+        assert delete_response.status_code == 204
+        assert (await record_instance.find_existing_record()) is None
+
+
+# 339 starting records
