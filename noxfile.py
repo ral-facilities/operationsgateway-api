ea056ba2c930f826d477d9aeca39528592b76bfb
moonraker595 <airkemp@gmail.com>
2025-03-06 16:39:54 +0000

    Updated comments

--- noxfile.py
+++ noxfile.py
@@ -6,7 +6,9 @@ code_locations = "operationsgateway_api", "test", "noxfile.py", "util"
 
 @nox.session(reuse_venv=True)
 def black(session):
+    # Use Poetry’s existing virtual environment
     session.env["POETRY_VIRTUALENVS_CREATE"] = "false"
+    # The dependencies should already be installed, but explicitly setting in case they're not
     session.run("poetry", "install", "--without", "simulated-data", external=True)
     args = session.posargs
     session.run("poetry", "run", "black", *code_locations, *args, external=True)
@@ -15,19 +17,14 @@ def black(session):
 @nox.session(reuse_venv=True)
 def lint(session):
     args = session.posargs or code_locations
-    # Use Poetry’s existing virtual environment
     session.env["POETRY_VIRTUALENVS_CREATE"] = "false"
-    # Ensure dependencies are installed
     session.run("poetry", "install", "--without", "simulated-data", external=True)
-    # Run flake8 using Poetry’s virtual environment
     session.run("poetry", "run", "flake8", *args, external=True)
 
 
 @nox.session(reuse_venv=True)
 def safety(session):
-    # Ensure Poetry uses its virtual environment
     session.env["POETRY_VIRTUALENVS_CREATE"] = "false"
-    # Install dependencies
     session.run("poetry", "install", "--without", "simulated-data", external=True)
     # Can't fix 70790 because epac data sim uses it
     session.run(
