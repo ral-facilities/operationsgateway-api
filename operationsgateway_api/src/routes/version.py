b8c469ecd52d6a9bdf4b7f3b54e3d6994121b175
kdu14933 <airkemp@gmail.com>
2025-04-11 14:04:36 +0000

    Now getting version from pyproject.toml

--- operationsgateway_api/src/routes/version.py
+++ operationsgateway_api/src/routes/version.py
@@ -1,9 +1,8 @@
+from importlib.metadata import version
 import logging
 
 from fastapi import APIRouter
-from fastapi.responses import JSONResponse
 
-from operationsgateway_api.src.config import Config
 from operationsgateway_api.src.error_handling import endpoint_error_handling
 
 log = logging.getLogger()
@@ -12,12 +11,11 @@ router = APIRouter()
 
 @router.get(
     "/version",
-    summary="Gets the deployed version hash of the API",
-    response_description="A hash to identify the GIT release",
+    summary="Gets the deployed version of the API",
+    response_description="A tag to identify the GIT release",
     tags=["Version"],
 )
 @endpoint_error_handling
 async def get_version():
-    version = Config.config.app.version
-    log.info("Returning API version: %s", version)
-    return JSONResponse(content={"version": version})
+    # The version is set in the pyproject.toml
+    return {"version": version("operationsgateway-api")}
